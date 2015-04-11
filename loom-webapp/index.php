f3f2ec8d8708b7b3d139172f5f93a5f2afe79515
Nick Cottrell <nick.a.cottrell@gmail.com>
2015-04-11 16:16:38 -0700

    added an index.php to the webapp

---
+++ loom-webapp/index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+</head>
+<body>
+
+<ul>
+	<li><a href="write.php">write</a></li>
+	<li><a href="edit.php">edit</a></li>
+	<li><a href="delete.php">delete</a></li>
+</ul>
+<hr />
+
+</body>
+</html>
